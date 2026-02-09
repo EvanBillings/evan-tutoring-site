b08c5f6cc5848b965b5902ad987be12036336214
Evan Billings <evancbillings@gmail.com>
2026-02-09 07:58:02 +0000

    Update Clerk to fix Edge Runtime error

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Define which routes are protected
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/admin(.*)'
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
